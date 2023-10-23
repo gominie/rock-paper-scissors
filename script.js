6cea66a6e2d0eb2561a4bcbe795e638f16a7b433
Gominie <109306648+gominie@users.noreply.github.com>
2023-10-23 17:50:17 +0100

    loop for 5 rounds

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
-const stringCollection = ["Rock","Paper","Scissors"]
-//computer choice
+const stringCollection = ["rock", "paper", "scissors"];
+
 function getComputerChoice(collection) {
-    const randomIndex = Math.floor(Math.random() * collection.length);
+  const randomIndex = Math.floor(Math.random() * collection.length);
   return collection[randomIndex];
 }
-const computerSelection = getComputerChoice(stringCollection)
 
-
-let playerSelection = prompt("Rock, paper or scissors");
 function playRound(playerSelection, computerSelection) {
-     if (!stringCollection.includes(playerSelection)) {
-        return "Invalid input. Please choose Rock, Paper, or Scissors."
-     }
-    if (playerSelection !== null) {
-        playerSelection = playerSelection.toLowerCase();
+if (playerSelection === null) {
+        return "You canceled the game.";
     }
 
-    if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Scissors" && computerSelection === "Paper") ||
-        (playerSelection === "Paper" && computerSelection === "Rock")   
-    ) {
-        return `${playerSelection} beats ${computerSelection}. You win!`;
-    } else {
-      return `${computerSelection} beats ${playerSelection}. You lose!`;
-    }
-    
+    playerSelection = playerSelection.toLowerCase();
+
+if (!stringCollection.includes(playerSelection)) {
+    return "Invalid input. Please choose rock, paper, or scissors.";
+  }
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return `${playerSelection} beats ${computerSelection}. You win!`;
+  } else {
+    return `${computerSelection} beats ${playerSelection}. You lose!`;
+  }
 }
 
-let result = playRound(playerSelection, computerSelection);
-console.log(result)
+function game(rounds) {
+  for (let i = 0; i < rounds; i++) {
+let playerSelection;
+while (true) {
+    playerSelection = prompt("Rock, paper, or scissors");
+if (playerSelection !== null) {
+                playerSelection = playerSelection.toLowerCase(); // Convert input to lowercase
+}
+if (stringCollection.includes(playerSelection)) {
+                break;
+}
+alert("Invalid input. Please choose rock, paper, or scissors.");
+        }
+    const computerSelection = getComputerChoice(stringCollection);
+    let result = playRound(playerSelection, computerSelection);
+    alert(result);
+  }
+}
+
+game(5);
+
