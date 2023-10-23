c31c92dd85d56a372138e29d45736def7df99d70
Gominie <109306648+gominie@users.noreply.github.com>
2023-10-23 17:02:10 +0100

    added player selection and result text

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const stringCollection = ["Rock","Paper","Scissors"]
+//computer choice
 function getComputerChoice(collection) {
     const randomIndex = Math.floor(Math.random() * collection.length);
   return collection[randomIndex];
 }
+const computerSelection = getComputerChoice(stringCollection)
 
-const computerChoice = getComputerChoice(stringCollection)
+
+let playerSelection = prompt("Rock, paper or scissors");
+function playRound(playerSelection, computerSelection) {
+     
+    if (playerSelection !== null) {
+        playerSelection = playerSelection.toLowerCase();
+    }
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Rock")   
+    ) {
+        return `${playerSelection} beats ${computerSelection}. You win!`;
+    } else {
+      return `${computerSelection} beats ${playerSelection}. You lose!`;
+    }
+    
+}
+
+let result = playRound(playerSelection, computerSelection);
+console.log(result)
